1c7ef2c4f9653e68ff4faf51cf79b62873a46a8b
Shakil <shuvroahmed959@gmail.com>
2023-08-26 17:31:14 +0600

    Data Driven Testing- Apache POI

--- TestNGFramework/src/test/java/dataDrivenTesting/TC_01_DataDrivenTesting.java
+++ TestNGFramework/src/test/java/dataDrivenTesting/TC_01_DataDrivenTesting.java
@@ -7,6 +7,12 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.poi.xssf.usermodel.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TC_01_DataDrivenTesting {
 
@@ -39,24 +45,40 @@ public class TC_01_DataDrivenTesting {
 		int ttlRows = ExcelWSheet.getLastRowNum() + 1;
 		int ttlCells = ExcelWSheet.getRow(0).getLastCellNum();
 
-		for (int currentRow = 0; currentRow < ttlRows; currentRow++) {
+		for (int currentRow = 1; currentRow < ttlRows; currentRow++) {
 
-			for (int currentCell = 0; currentCell < ttlCells; currentCell++) {
+			//for (int currentCell = 0; currentCell < ttlCells; currentCell++) {
 
-				System.out.println(ExcelWSheet.getRow(currentRow).getCell(currentCell).toString());
-
+            //Launch Chrome Browser
+				WebDriverManager.chromedriver().setup();
+				WebDriver driver = new ChromeDriver();
+				driver.get("https://www.saucedemo.com/");
+//				driver.quit();
+				
+				//enter username
+				driver.findElement(By.id("user-name")).sendKeys(ExcelWSheet.getRow(currentRow).getCell(0).toString());
+				//password
+				driver.findElement(By.id("password")).sendKeys(ExcelWSheet.getRow(currentRow).getCell(1).toString());
+				driver.findElement(By.id("login-button")).click();
+				
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				driver.quit();
+				
+//				System.out.println(ExcelWSheet.getRow(currentRow).getCell(currentCell).toString());
+//
 //				System.out.print("\t");
 			}
-			System.out.println();
+			
 		}
 		
 
-		try {
-			ExcelWBook.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 	}
 
-}
+
