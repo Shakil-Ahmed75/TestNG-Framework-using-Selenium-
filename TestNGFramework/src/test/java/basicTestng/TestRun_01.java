998f6df7eb8159865e4d5ed96f63fb8debc80d36
Shakil <shuvroahmed959@gmail.com>
2023-10-03 16:09:13 +0600

    Run TestNG

---
+++ TestNGFramework/src/test/java/basicTestng/TestRun_01.java
@@ -0,0 +1,17 @@
+package basicTestng;
+
+import org.testng.annotations.Test;
+
+public class TestRun_01 {
+	@Test
+	public void testRun01() {
+		System.out.println("Hello_01");
+	}
+	
+	@Test
+	public void testRun02() {
+		System.out.println("Hello_02");
+		
+	}
+
+}
---
+++ TestNGFramework/src/test/java/basicTestng/TestRun_02.java
@@ -0,0 +1,11 @@
+package basicTestng;
+
+import org.testng.annotations.Test;
+
+public class TestRun_02 {
+	@Test
+	public void testrun_03() {
+		System.out.println("Hello_03");
+	}
+
+}
