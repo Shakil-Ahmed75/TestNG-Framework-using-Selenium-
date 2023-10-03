87608196828603702a3b7c1704d3b15ec17b2fee
Shakil <shuvroahmed959@gmail.com>
2023-10-03 21:21:54 +0600

    exclude testNG

--- TestNGFramework/src/test/java/basicTestng/TestRun_02.java
+++ TestNGFramework/src/test/java/basicTestng/TestRun_02.java
@@ -8,4 +8,18 @@ public class TestRun_02 {
 		System.out.println("Hello_03");
 	}
 
+	@Test
+	public void weblogin() {
+		System.out.println("WebLogin");
+	}
+	
+	@Test
+	public void moblielogin() {
+	 System.out.println("MoblieLogin");
+	}
+	
+	@Test
+	public void apiTesting() {
+		System.out.println("APITesting");
+	}
 }
